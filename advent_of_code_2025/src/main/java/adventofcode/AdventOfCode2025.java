23fbec995a482db8e27a13da5afe06a363c876c8
Jonathon Tait <jttait@gmail.com>
2026-01-11 11:01:21 +0000

    local changes

---
+++ advent_of_code_2025/src/main/java/adventofcode/AdventOfCode2025.java
@@ -0,0 +1,8 @@
+package adventofcode;
+
+public class AdventOfCode2025 {
+    public static void main(String[] args) {
+        int = Integer.parseInt()
+        System.out.println("Hello, world!");
+    }
+}
